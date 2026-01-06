b4b39b2ab56a1e3e32670ec70117164877eb687e
ilhambonardoo <ilhambonardo123@gmail.com>
2026-01-07 02:05:06 +0700

    setup project untuk firebase admin dan hapus untuk unitesting sementara

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["firebase-admin"],
+
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+
+  reactStrictMode: true,
+  poweredByHeader: false,
 };
 
 export default nextConfig;
